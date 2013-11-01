bc3c5e387d330ba63b1c348fe8f459570ee78b72
Christopher Fredericks <zsouthboy+git@gmail.com>
2013-11-01 07:21:58 -0400

    completely forgot to make /jpg use the newly created function

--- raspistill_http.py
+++ raspistill_http.py
@@ -19,17 +19,14 @@ class index:
 
 class jpg:
     def GET(self):
-        # that's right, read the entire file every time. it's in ram, it'll be ok
-	if ok_to_capture():
-                subprocess.call(CAPTURE_COMMAND)
-                open(LOCKFILE, "w").write(str(time.time()))
         web.header("Content-Type", "image/jpeg")
-        return out
+        return get_image()
 
 def ok_to_capture():
+    # that's right, read the entire file every time. it's in ram, it'll be ok
+    # we're not killing the poor raspberry pi sdcard
     if not os.path.exists(LOCKFILE):
         open(LOCKFILE, "w").write(str(time.time()))
-    # same, he's in ram, we're not killing the poor raspberry pi sdcard
     if time.time() > (float(open(LOCKFILE).read()) + MINIMUM_TIME_BETWEEN_CAPTURES):
         return True
     return False
