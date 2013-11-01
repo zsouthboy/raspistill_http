0a69993487fa6ea3640bdfccb8cde213f4ddacde
Christopher Fredericks <zsouthboy+git@gmail.com>
2013-11-01 06:50:01 -0400

    changed to a file-based check of last run of raspistill since I think multiple users can cause it to happen more frequently than intended

--- raspistill_http.py
+++ raspistill_http.py
@@ -4,13 +4,12 @@ import sys
 import subprocess
 from const import *
 import time
+import os.path
 
 urls = (
     "/", "index",
     "/jpg", "jpg"
 )
-# HACK. ugh
-LAST_CAPTURE = time.time() - MINIMUM_TIME_BETWEEN_CAPTURES
 
 
 class index:
@@ -20,15 +19,21 @@ class index:
 
 class jpg:
     def GET(self):
-        # HACK
-        global LAST_CAPTURE
-	if time.time() > (LAST_CAPTURE + MINIMUM_TIME_BETWEEN_CAPTURES):
+        # that's right, read the entire file every time. it's in ram, it'll be ok
+	if ok_to_capture():
                 subprocess.call(CAPTURE_COMMAND)
-                LAST_CAPTURE = time.time()
+                open(LOCKFILE, "w").write(str(time.time()))
         web.header("Content-Type", "image/jpeg")
         out = open(PATH_TO_LATEST_JPG, "rb").read()
         return out
 
+def ok_to_capture():
+    if not os.path.exists(LOCKFILE):
+        open(LOCKFILE, "w").write(str(time.time()))
+    # same, he's in ram, we're not killing the poor raspberry pi sdcard
+    if time.time() > (float(open(LOCKFILE).read()) + MINIMUM_TIME_BETWEEN_CAPTURES):
+        return True
+    return False
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
