d7d0660d93a57d388208024e25ad33a749427d9f
Christopher Fredericks <zsouthboy+git@gmail.com>
2013-10-31 22:35:53 -0400

    initial js to refresh image. investigating camera crash

--- const.py
+++ const.py
@@ -7,12 +7,18 @@ INDEX_HTML = """
   </head>
   <body>
     <h1>Camera</h1>
-    <img src="/jpg">
+    <img src="/jpg" id="webcam_view">
+    <script>
+    var im = document.getElementById("webcam_view");
+    setInterval(function(){
+        im.src = "/jpg?rand=" + Math.random(); 
+    }, 1000);
+    </script>
   </body>
 </html>
 
 """
 
 PATH_TO_LATEST_JPG = "/run/shm/1.jpg"
-CAPTURE_COMMAND = ["/usr/bin/raspistill", "-t", "500", "-n", "-w", "800", "-h", "600", "-vf", "-o", "/run/shm/1.jpg"]
+CAPTURE_COMMAND = ["/usr/bin/raspistill", "-t", "200", "-n", "-w", "800", "-h", "600", "-vf", "-o", "/run/shm/1.jpg"]
 
