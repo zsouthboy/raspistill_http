54e12445987de519d7d5a12909b2e53dd95ae8de
Christopher Fredericks <zsouthboy+git@gmail.com>
2013-11-02 20:26:33 -0400

    tested with these settings for 48+ hours without a hang or problem

--- const.py
+++ const.py
@@ -1,7 +1,7 @@
 PATH_TO_LATEST_JPG = "/run/shm/1.jpg"
-CAPTURE_COMMAND = ["/usr/bin/raspistill", "-t", "200", "-n", "-w", "800", "-h", "600", "-vf", "-o", "/run/shm/1.jpg"]
+CAPTURE_COMMAND = ["/usr/bin/raspistill", "-t", "500", "-n", "-w", "400", "-h", "300", "-vf", "-o", "/run/shm/1.jpg"]
 MINIMUM_TIME_BETWEEN_CAPTURES = 5  # seconds
-JSREFRESH = 1000
+JSREFRESH = 6000
 LOCKFILE = "/run/shm/raspistill_http.last"
 INDEX_HTML = """
 <!DOCTYPE html>
